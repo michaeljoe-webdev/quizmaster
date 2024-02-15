c07e0a395c808685d425c593fe7c6ef0b2ad4ab6
maikerulearncoding <maikerulearncoding@gmail.com>
2024-02-16 06:03:12 +0800

    rebuild to old

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,20 @@
 <template>
   <img alt="Quiz Master logo" src="./assets/qm-logo.png" height="150px">
-  <button @click="signIn">Sign in with Google</button>
-  <!-- <button @click="signInWithGoogle">Sign in with Google</button> -->
-
   <QuizMaster msg="Welcome to Quiz Master!"/>
   <!-- <CongratsAnimation/> -->
 
 </template>
 
 <script>
-// import { GoogleAuth } from 'google-auth-library';
-import googleAuth from '@/services/googleAuth';
 import QuizMaster from './components/QuizMaster.vue';
 // import CongratsAnimation from './components/CongratsAnimation.vue'
-
 
 export default {
   name: 'App',
   components: {
     QuizMaster,
     // CongratsAnimation
-  },
-  methods: {
-    async signIn() {
-      await googleAuth.signIn();
-    },
-    // async signInWithGoogle() {
-    //   const auth = new GoogleAuth();
-    //   const client = await auth.getClient({
-    //     scopes: ['email', 'profile'], // Define the scopes for accessing user information
-    //   });
-      
-    //   // Use the client to sign in the user
-    //   const ticket = await client.verifyIdToken({
-    //     idToken: 'YOUR_ID_TOKEN_FROM_GOOGLE', // Replace with the actual ID token received from the frontend
-    //     audience: 'YOUR_CLIENT_ID.apps.googleusercontent.com', // Replace with your OAuth client ID
-    //   });
-      
-    //   const payload = ticket.getPayload();
-    //   console.log(payload); // Log the user's profile information
-    // },
-  },
+  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -3,6 +3,5 @@ import App from './App.vue'
 // import CongratsAnimation from './components/CongratsAnimation.vue'
 import Particles from "vue3-particles"
 
-
 // createApp(CongratsAnimation).use(Particles).mount('#animation');
 createApp(App).use(Particles).mount('#app');
